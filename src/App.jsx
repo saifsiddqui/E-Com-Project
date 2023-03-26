f80dfd762b122361ac69166ea7e7b20014ba6553
Ajay_Bains <ajay.bains@codvo.ai>
2023-03-26 20:55:59 +0530

    grid added to categories section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import "./App.css";
 import Hero from "./components/Hero/Hero";
+import CategoriesSection from "./components/LandingPageCategoriesSection/CategoriesSection";
 
 const App = () => {
   const [users, setUsers] = useState();
   return (
     <>
       <Hero />
+      <CategoriesSection/>
       {/* {users ? (
         <div>welcome to the UI dashboard</div>
       ) : (
---
+++ src/components/LandingPageCategoriesSection/CategoriesSection.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./CategoriesSection.css";
+
+const CategoriesSection = () => {
+  return (
+    <div className="categories">
+      <div></div>
+      <div className="grid-col-span-2"></div>
+      <div className="grid-col-span-2"></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default CategoriesSection;
